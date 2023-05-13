b6231ecddbe2bb99841ac561ca98e6681a73b80f
Henry Augusta <henryaugusta4@gmail.com>
2023-05-13 15:18:28 +0700

    first commit

---
+++ lib/util/my_colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Use for the background color of your app's primary components (e.g., screens, cards)
+  static const Color lightPrimaryColor = Color(0xFFFFCDD2);
+
+  // Use for the primary color of your app's theme (e.g., app bar, buttons)
+  static const Color primaryColor = Color(0xFFF44336);
+
+  // Use for text and icons on light backgrounds (e.g., button text, icon color)
+  static const Color textIconsColor = Color(0xFFFFFFFF);
+
+  // Use for accent colors or secondary elements (e.g., floating action button, progress indicator)
+  static const Color accentColor = Color(0xFF9E9E9E);
+
+  // Use for primary text color on light backgrounds (e.g., headline, body text)
+  static const Color primaryTextColor = Color(0xFF212121);
+
+  // Use for secondary text color on light backgrounds (e.g., subhead, captions)
+  static const Color secondaryTextColor = Color(0xFF757575);
+
+  // Use for dividing lines or borders between elements (e.g., list divider)
+  static const Color dividerColor = Color(0xFFBDBDBD);
+}
